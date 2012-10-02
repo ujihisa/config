40e3a7d5e80ab81a2b204e6ad4d6a01bc2d0dfda
ujihisa <ujihisa@gmail.com>
2012-10-02 00:34:44 -0700

    good-bye legacy

--- _pythonrc.py
+++
@@ -1,6 +0,0 @@
-import rlcompleter
-import readline
-readline.parse_and_bind("bind -v")
-readline.parse_and_bind("bind ^I rl_complete")
-readline.parse_and_bind("bind ^N rl_complete")
-readline.parse_and_bind("bind ^P rl_complete")
